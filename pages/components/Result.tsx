80493734e629e27bb7414034592f676f0cfa4957
rigrergl <rigrergl@gmail>
2022-11-17 20:59:21 -0600

    added some headers

--- pages/components/Result.tsx
+++ pages/components/Result.tsx
@@ -50,6 +50,8 @@ const Result = (props: Props) => {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        "Access-Control-Allow-Origin" :  "*",    
+        "Access-Control-Allow-Headers" : "Origin, X-Requested-With, Content-Type, Accept"
       },
       body: JSON.stringify({
         listingId: props.listing_id,
