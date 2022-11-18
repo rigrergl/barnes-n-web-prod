134b50ad315adf8069b1f7caed653d00880495a5
rigrergl <rigrergl@gmail>
2022-11-17 19:12:43 -0600

    added input sanitization module

---
+++ lib/validateInput.ts
@@ -0,0 +1,7 @@
+export default (input: String) : boolean => {
+    if (input.includes("'") || input.includes("-") || input.includes(";")) {
+        return false;
+    } else {
+        return true;
+    }
+}
